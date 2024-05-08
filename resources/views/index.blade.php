e6ef3c40a9808528513591bb04545ad76e656c3b
ruth manurung <ruthgracearlyanamanurung@gmail.com>
2024-05-08 10:30:07 +0700

    Ngetest folder bang

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Klinik RH61</title>
-    <link rel="stylesheet" href="{{ asset('./assets/css/app.css') }}">
-</head>
-<body>
-    <h1>Hello World</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+
+<div class="container">
+    <div class="col-lg-12 pt-3">
+        <h6>Home Test </h6>
+        <hr class="mt-0">
+    </div>
+</div>
+
+@endsection
