85dccbdca119a031914eb49e271f77d1ab81d11f
ruth manurung <ruthgracearlyanamanurung@gmail.com>
2024-05-11 09:30:50 +0700

    index tampilan deskop

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,41 @@
-@extends('layouts.main')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Klinik RH61</title>
+    <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="image/png">
+    <link rel="stylesheet" href="{{ asset('./assets/css/app.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+</head>
 
-@section('container')
+<body class="font-body">
+    <div class="container-fluid">
+        <div class="w-full h-96 relative rounded-b-4xl bg-[#E3EBF3]">
+            <div class="flex absolute left-4 top-4 w-36 h-36">
+                <img src="{{ asset('assets/img/logo.png') }}" alt="">
+            </div>
+            <div class="flex absolute right-1 bottom-0 w-100 h-80">
+                <img src="{{ asset('assets/img/Picture-Doctor.png') }}" alt="">
+            </div>
+        </div>
 
-<div class="container">
-    <div class="col-lg-12 pt-3">
-        <h6>Home Test </h6>
-        <hr class="mt-0">
+        <div class="flex justify-center">
+            <div class="my-12 font-body font-bold text-[#0a0a0a] text-3xl">
+                <h1>Manage Your Health and</h1>
+                <div class="flex justify-center">
+                    <span class="block">Happy Future</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex justify-center mb-2">
+            <button class="bg-[#222C67] hover:bg-[#525985] text-[#f5f5f5] font-bold py-2 px-4 border-b-4 border-[#222C67] hover:border-[#525985] rounded font-body">
+                Get Started
+            </button>
+        </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
