2835bda0280dffb42534ea46d568e4cd26ceaa8a
luthfi2603 <luthfim904@gmail.com>
2024-05-11 09:58:43 +0700

    perbaiki index

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Klinik RH61</title>
-    <link rel="stylesheet" href="{{ asset('./assets/css/app.css') }}">
-</head>
-<body>
-    <div class="flex flex-col items-center bg-blue-400 text-white h-screen justify-center">
-        <p class="text-4xl">Dashboard</p>
-        <div class="mt-4">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="px-4 py-2 bg-white rounded-xl text-blue-400 mb-4 mt-1">Keluar</button>
-            </form>
-        </div>
+@extends('layouts.main')
+
+@section('container')
+
+<div class="container">
+    <div class="col-lg-12 pt-3">
+        <h6>Home Test </h6>
+        <hr class="mt-0">
     </div>
-</body>
-</html>
+</div>
+
+@endsection
