39b89880f80ecfbc1474ad24f7f624be1291a2f0
ruth manurung <ruthgracearlyanamanurung@gmail.com>
2024-05-14 14:29:09 +0700

    resolved conflic

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,9 +11,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 
-<body class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-    <div class="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-        <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+    <body class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+        <div class="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+          <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+            
             <div class="mt-12 flex flex-col items-center">
                 <h2 class="text-3xl font-bold text-[#222C67]">
                     Selamat Datang
